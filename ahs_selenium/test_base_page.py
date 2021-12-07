a44f4c6f6c0af3ca116d434d624bc40fd05065e4
kadirovgm <kadirovgm@gmail.com>
2021-12-07 22:07:53 +0300

    clean code

--- ahs_selenium/test_base_page.py
+++ ahs_selenium/test_base_page.py
@@ -19,21 +19,24 @@ class TestBasePageActions:
         self.page.login_new_user()
 
     def test_user_can_go_to_positions(self, browser):
-        page_base = BasePage(browser, browser.current_url)
-        page_base.go_to_positions_page()
         page_positions = PositionsPage(browser, browser.current_url)
+        page_positions.go_to_positions_page()
         page_positions.should_be_positions_page_text()
 
     def test_user_can_go_to_pool(self, browser):
-        page_base = BasePage(browser, browser.current_url)
-        page_base.go_to_pool_page()
         page_pool = PoolPage(browser, browser.current_url)
+        page_pool.go_to_pool_page()
         page_pool.should_be_pool_page_text()
 
     def test_user_can_go_to_clients_projects(self, browser):
-        page_base = BasePage(browser, browser.current_url)
-        page_base.go_to_clients_projects_page()
         page_clients_projects = ClientsProjectsPage(browser, browser.current_url)
+        page_clients_projects.go_to_clients_projects_page()
         page_clients_projects.should_be_clients_projects_page_text()
+
+    def test_user_can_go_to_reports(self, browser):
+        ...
+
+    def test_user_can_go_to_help_center(self, browser):
+        ...
 
 
