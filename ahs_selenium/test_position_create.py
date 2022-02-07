de3872939c0cd0a7e8c59c354fd80f3c5e68f56b
kadirovgm <kadirovgm@gmail.com>
2022-02-08 00:20:29 +0300

    new files

--- ahs_selenium/test_position_create.py
+++ ahs_selenium/test_position_create.py
@@ -4,10 +4,29 @@ from pages.login_page import LoginPage
 from urls import Urls
 
 @pytest.mark.skip
-class TestCreateClientPosition:
+class TestClientProjectPosition:
     @pytest.fixture(scope="class", autouse=True)
     def setup(self, browser):
         page = LoginPage(browser, Urls.LINK_LOGIN_PAGE)
         page.open()
         time.sleep(0.5)
         page.login_new_user(email="admin@admin.com", password="P@ssw0rd1")
+    
+    def test_create_client_project_position(self, browser):
+        ...
+
+@pytest.mark.skip
+class TestInternalProjectPosition:
+    ...
+
+@pytest.mark.skip
+class TestBenchPosition:
+    ...
+
+@pytest.mark.skip
+class TestPreOfferPosition:
+    ...
+
+@pytest.mark.skip
+class TestTraineePosition:
+    ...
