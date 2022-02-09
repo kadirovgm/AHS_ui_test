68e9c259a77e6c199062adaa5aabe3753439f6bc
kadirovgm <kadirovgm@gmail.com>
2022-02-09 20:15:10 +0300

    conftest pep8

--- ahs_selenium/conftest.py
+++ ahs_selenium/conftest.py
@@ -22,23 +22,24 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")  # "function" if want to initialize on every function
 def browser(request):
+    # TODO include to dockerfile
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    # DRIVER_BIN = os.path.join(PROJECT_ROOT, "../bin/chromedriver")  # execute in mac
+    # DRIVER_BIN = os.path.join(PROJECT_ROOT, "../bin/chromedriver")    # execute in mac
     DRIVER_BIN = os.path.join(PROJECT_ROOT, "../bin/chromedriver.exe")  # execute in windows
 
     ser = Service(DRIVER_BIN)
 
     browser_name = request.config.getoption("browser_name")
-    # user_language = request.config.getoption("language")
+    # user_language = request.config.getoption("language")        # if want to launch eng version of site
     user_language = None
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
 
         print("\nStart chrome browser for test..")
-        browser = webdriver.Chrome(options=options, service=ser) # executable_path=DRIVER_BIN - deprecated
-        browser.maximize_window()                       # maximize window
-        browser.implicitly_wait(3)                      # implicitly wait
+        browser = webdriver.Chrome(options=options, service=ser)  # executable_path=DRIVER_BIN - deprecated
+        browser.maximize_window()                                 # maximize window
+        browser.implicitly_wait(3)                                # implicitly wait
 
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
@@ -51,7 +52,6 @@ def browser(request):
 
     print("\nquit browser..")
     browser.quit()
-
 
 # Basic browser
 # @pytest.fixture(scope="function")
