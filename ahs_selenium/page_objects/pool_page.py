be6a2784a71ad4ff328395c6e663b5c02fdea6cf
kadirovgm <kadirovgm@gmail.com>
2022-02-23 15:21:29 +0300

    pool locators refactoring part 2

--- ahs_selenium/page_objects/pool_page.py
+++ ahs_selenium/page_objects/pool_page.py
@@ -18,14 +18,15 @@ class PoolPage(BasePage):
         add_person = self.browser.find_element(*PoolPageLocators.ADD_PERSON)
         add_person.click()
 
+    """Add person modal opened"""
     def add_person_modal_should_be_opened(self):
         assert self.is_element_present(*CreatePersonModalLocators.CREATE_NEW_PERSON_TEXT), \
             "Create person modal incorrect appeared"
 
-    """Internal tab"""
+    """Internal tab [FIELDS]"""
     def should_be_correct_fields_internal(self):
         internal_fields_locators = \
-            ["NAME_i", "TYPE_i", "ROLE_i", "SKILLS_i", "CITY_COUNTRY_i", "OFFICE_i", "ENG_LEVEL_i",
+            ["NAME", "TYPE_i", "ROLE_i", "SKILLS_i", "CITY_COUNTRY_i", "OFFICE_i", "ENG_LEVEL_i",
              "VISA_i", "ACTIVE_PROJECTS_i", "HR_i"]
         internal_fields_locators = [eval("PoolPageLocators." + i) for i in internal_fields_locators]
         internal_fields_text = \
@@ -33,6 +34,15 @@ class PoolPage(BasePage):
              "Visa status", "Active projects", "HR"]
         self.checking_fields_for_naming(internal_fields_locators, internal_fields_text)
 
+    """External (Blacklist) tab [FIELDS]"""
+    def should_be_correct_fields_external_blacklist(self):
+        external_fields_locators = \
+            ["NAME", "ROLE_e", "SKILLS_e", "CITY_COUNTRY_e", "OFFICE_e", "ENG_LEVEL_e", "VISA_e", "HR_e"]
+        external_fields_locators = [eval("PoolPageLocators." + i) for i in external_fields_locators]
+        external_fields_text = ["Name", "Roles", "Skills", "City | Country", "Office", "Eng. level", "Visa status", "HR"]
+        self.checking_fields_for_naming(external_fields_locators, external_fields_text)
+    
+    """Internal tab [FILTERS]"""
     def should_be_filters_internal(self):
         internal_filters_locators = \
             ["F_LABEL", "F_TYPE_i", "F_ROLE_i", "F_SKILLS_i", "F_CITY_i", "F_OFFICE_i",
@@ -41,14 +51,7 @@ class PoolPage(BasePage):
         for locator in internal_filters_locators:
             assert self.is_element_present(*locator), f"{locator} filter doesn't present!"
 
-    """External (Blacklist) tab"""
-    def should_be_correct_fields_external_blacklist(self):
-        external_fields_locators = \
-            ["NAME_e", "ROLE_e", "SKILLS_e", "CITY_COUNTRY_e", "OFFICE_e", "ENG_LEVEL_e", "VISA_e", "HR_e"]
-        external_fields_locators = [eval("PoolPageLocators." + i) for i in external_fields_locators]
-        external_fields_text = ["Name", "Roles", "Skills", "City | Country", "Office", "Eng. level", "Visa status", "HR"]
-        self.checking_fields_for_naming(external_fields_locators, external_fields_text)
-
+    """External (Blacklist) tab [FILTERS]"""
     def should_be_filters_external_blacklist(self):
         external_filters_locators = \
             ["F_LABEL", "F_ROLE_e", "F_SKILLS_e", "F_CITY_e", "F_OFFICE_e", "F_ENG_e", "F_VISA_e", "F_HR_e"]
@@ -70,19 +73,16 @@ class PoolPage(BasePage):
     def filter_label(self, label, tab):
         locator_label_select = ...
         if label == "Bench" and tab == "Internal":
-            locator_label_select = PoolPageLocators.F_LABEL_i_bench
+            locator_label_select = PoolPageLocators.F_LABEL_bench
         elif label == "Pre-offer" and tab == "External":
-            locator_label_select = PoolPageLocators.F_LABEL_e_PRE_OFFER
-        elif label == "Dismiss" and tab == "External":
-            locator_label_select = PoolPageLocators.F_LABEL_e_Dismiss
-        elif label == "Dismiss" and tab == "Blacklist":
-            locator_label_select = PoolPageLocators.F_LABEL_b_Dismiss
+            locator_label_select = PoolPageLocators.F_LABEL_pre_offer
+        elif label == "Dismiss" and (tab == "External" or tab == "Blacklist"):
+            locator_label_select = PoolPageLocators.F_LABEL_dismiss
         else:
             raise AssertionError("Please specify as label - 'Bench', 'Pre-offer', 'Dismiss'")
 
         # Common for all tabs locators
         locator_label_filter = PoolPageLocators.F_LABEL
-        locator_label_ok = PoolPageLocators.F_LABEL_OK
         locator_label_expected = PoolPageLocators.FIRST_PERSON_LABEL
 
         # open filter
@@ -93,7 +93,7 @@ class PoolPage(BasePage):
             _ = self.browser.find_element(*locator_label_select).click()
         else:
             raise AssertionError(f"There is no {label} label")
-        _ = self.browser.find_element(*locator_label_ok).click()
+        _ = self.browser.find_element(*PoolPageLocators.F_OK).click()
 
         # check filtering (label, expected label)
         self.is_filter_works(label, locator_label_expected)
@@ -101,8 +101,7 @@ class PoolPage(BasePage):
     """[FILTERING]  Reset filter by label [All tabs]"""
     def reset_filter_label(self):
         label_filter_locator = PoolPageLocators.F_LABEL
-        reset_locator = PoolPageLocators.F_LABEL_RESET
-        self.is_filter_reset(label_filter_locator, reset_locator)
+        self.is_filter_reset(label_filter_locator)
 
     """[FILTERING] Check Filter by type [Internal]"""
     def filter_type_internal(self, person_type):
@@ -110,10 +109,10 @@ class PoolPage(BasePage):
         locator_type = ...
         if person_type == "Long-term":
             locator_type = PoolPageLocators.F_TYPE_i_LONG_TERM
-        elif person_type == "Contractor":
-            locator_type = PoolPageLocators.F_TYPE_i_CONTRACTOR
         elif person_type == "Short-term":
             locator_type = PoolPageLocators.F_TYPE_i_SHORT_TERM
+        elif person_type == "Contractor":
+            locator_type = PoolPageLocators.F_TYPE_i_CONTRACTOR
         else:
             raise AssertionError("Please specify as internal person types - 'Long-term', 'Contractor', 'Short-term'")
 
@@ -125,7 +124,7 @@ class PoolPage(BasePage):
             _ = self.browser.find_element(*locator_type).click()
         else:
             raise AssertionError(f"There is no {person_type} label")
-        _ = self.browser.find_element(*PoolPageLocators.F_TYPE_i_OK).click()
+        _ = self.browser.find_element(*PoolPageLocators.F_OK).click()
 
         # check filtering
         self.is_filter_works(person_type, PoolPageLocators.FIRST_PERSON_TYPE_i)
@@ -133,22 +132,18 @@ class PoolPage(BasePage):
     """[FILTERING] Reset filter by person type [Internal]"""
     def reset_filter_type_internal(self):
         type_filter_locator = PoolPageLocators.F_TYPE_i
-        reset_locator = PoolPageLocators.F_TYPE_i_RESET
-        self.is_filter_reset(type_filter_locator, reset_locator)
+        self.is_filter_reset(type_filter_locator)
 
     """[FILTERING] Check Filter by role [All tabs]"""
     def filter_role(self, role, tab):
         if tab == "Internal":
             role_filter = PoolPageLocators.F_ROLE_i
-            role_select = PoolPageLocators.F_ROLE_i_SELECT
-            role_ok = PoolPageLocators.F_ROLE_i_OK
         elif tab == "External" or tab == "Blacklist":
             role_filter = PoolPageLocators.F_ROLE_e
-            role_select = PoolPageLocators.F_ROLE_e_SELECT
-            role_ok = PoolPageLocators.F_ROLE_e_OK
         else:
             raise AssertionError("Incorrect tab-name, please specify as: 'Internal', 'External' or 'Blacklist'")
         expected_role = PoolPageLocators.FIRST_PERSON_ROLE
+        role_select = PoolPageLocators.F_ROLE_SELECT
         # Open filter by role
         self.click_to_filter_by(role_filter)
         # Filtering
@@ -157,7 +152,7 @@ class PoolPage(BasePage):
             role_input.send_keys(role, Keys.ENTER)
         else:
             raise AssertionError(f"There is no input for filtering by role")
-        _ = self.browser.find_element(*role_ok).click()
+        _ = self.browser.find_element(*PoolPageLocators.F_OK).click()
         # Check filtering
         self.is_filter_works(role, expected_role)
 
@@ -165,14 +160,11 @@ class PoolPage(BasePage):
     def reset_filter_role(self, tab):
         if tab == "Internal":
             role_filter_locator = PoolPageLocators.F_ROLE_i
-            reset_locator = PoolPageLocators.F_ROLE_i_RESET
         elif tab == "External" or tab == "Blacklist":
             role_filter_locator = PoolPageLocators.F_ROLE_e
-            reset_locator = PoolPageLocators.F_ROLE_e_RESET
         else:
             raise AssertionError("Incorrect tab-name, please specify as: 'Internal', 'External' or 'Blacklist'")
-
-        self.is_filter_reset(role_filter_locator, reset_locator)
+        self.is_filter_reset(role_filter_locator)
 
     """[FILTERING] Filter by Skills [All tabs]"""
     def filter_skills(self, skill, tab, loading_time=0.7):
@@ -187,7 +179,6 @@ class PoolPage(BasePage):
         # Common for all tabs locators
         skill_select = PoolPageLocators.F_SKILLS_SELECT
         grade_select = PoolPageLocators.F_GRADE_SELECT
-        skill_ok = PoolPageLocators.F_SKILLS_OK
 
         # Open filter by skills
         self.click_to_filter_by(skill_filter)
@@ -202,7 +193,7 @@ class PoolPage(BasePage):
             ActionChains(self.browser).move_to_element(grade).click(grade).send_keys(Keys.DOWN + Keys.ENTER).perform()
         else:
             raise AssertionError(f"There is no input for filtering by Skill and Grade")
-        _ = self.browser.find_element(*skill_ok).click()
+        _ = self.browser.find_element(*PoolPageLocators.F_OK).click()
 
         # Check filtering
         self.is_filter_works(skill, expected_skill)
@@ -215,8 +206,7 @@ class PoolPage(BasePage):
             skill_filter_locator = PoolPageLocators.F_SKILLS_e
         else:
             raise AssertionError("Incorrect tab-name, please specify as: 'Internal', 'External' or 'Blacklist'")
-        reset_locator = PoolPageLocators.F_SKILLS_RESET
-        self.is_filter_reset(skill_filter_locator, reset_locator)
+        self.is_filter_reset(skill_filter_locator)
 
     """[FILTERING] Filter by City [All tabs]"""
     def filter_city(self, country, city, tab, loading_time=0.7):
@@ -230,7 +220,6 @@ class PoolPage(BasePage):
             raise AssertionError("Incorrect tab-name, please specify as: 'Internal', 'External' or 'Blacklist'")
         locator_country_select = PoolPageLocators.F_CITY_COUNTRY_SELECT
         locator_city_select = PoolPageLocators.F_CITY_CITY_SELECT
-        locator_city_ok = PoolPageLocators.F_CITY_OK
 
         # Open filter by skills
         self.click_to_filter_by(city_filter_locator)
@@ -247,7 +236,7 @@ class PoolPage(BasePage):
             city_input.send_keys(Keys.ENTER)
         else:
             raise AssertionError("There is no input for filtering by City and Country")
-        _ = self.browser.find_element(*locator_city_ok).click()
+        _ = self.browser.find_element(*PoolPageLocators.F_OK).click()
 
         # Check filtering
         self.is_filter_works(city, locator_expected_city)
@@ -260,8 +249,7 @@ class PoolPage(BasePage):
             city_filter_locator = PoolPageLocators.F_CITY_e
         else:
             raise AssertionError("Incorrect tab-name, please specify as: 'Internal', 'External' or 'Blacklist'")
-        reset_locator = PoolPageLocators.F_CITY_RESET
-        self.is_filter_reset(city_filter_locator, reset_locator)
+        self.is_filter_reset(city_filter_locator)
 
     """[Filtering] Filter by office [All tabs]"""
     def filter_office(self, office, tab):
@@ -274,7 +262,6 @@ class PoolPage(BasePage):
         else:
             raise AssertionError("Incorrect tab-name, please specify as: 'Internal', 'External' or 'Blacklist'")
         office_select = PoolPageLocators.F_OFFICE_SELECT
-        office_ok = PoolPageLocators.F_OFFICE_OK
         # Open filter by role
         self.click_to_filter_by(office_filter)
         # Filtering
@@ -283,7 +270,7 @@ class PoolPage(BasePage):
             role_input.send_keys(office, Keys.ENTER)
         else:
             raise AssertionError(f"There is no input for filtering by office")
-        _ = self.browser.find_element(*office_ok).click()
+        _ = self.browser.find_element(*PoolPageLocators.F_OK).click()
         # Check filtering
         self.is_filter_works(office, expected_office)
 
@@ -295,8 +282,7 @@ class PoolPage(BasePage):
             office_filter_locator = PoolPageLocators.F_OFFICE_e
         else:
             raise AssertionError("Incorrect tab-name, please specify as: 'Internal', 'External' or 'Blacklist'")
-        reset_locator = PoolPageLocators.F_OFFICE_RESET
-        self.is_filter_reset(office_filter_locator, reset_locator)
+        self.is_filter_reset(office_filter_locator)
 
     #### Additional methods for filtering ####
 
@@ -338,10 +324,11 @@ class PoolPage(BasePage):
             raise AssertionError(f"Probably there is no person with {str(filter_object)}: {filter_object}")
 
     """*Reset any filters [All tabs]*"""
-    def is_filter_reset(self, filter_locator, reset_locator):
+    def is_filter_reset(self, filter_locator):
         # open filter
         self.click_to_filter_by(filter_locator)
         # resetting
+        reset_locator = PoolPageLocators.F_RESET
         if self.waiting_for_element_present(*reset_locator):
             if self.browser.find_element(*reset_locator).is_enabled():
                 self.browser.find_element(*reset_locator).click()
