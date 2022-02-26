e6db5c0d2afba91a0513a0bf648814574e3a22c2
kadirovgm <kadirovgm@gmail.com>
2022-02-26 22:40:46 +0300

    actual version

--- ahs_selenium/test_pool_page.py
+++ ahs_selenium/test_pool_page.py
@@ -72,54 +72,54 @@ class TestPoolPageInternalFiltering:
         internal = PoolPage(browser, Urls.POOL_INTERNAL)
         internal.open()
 
-    # """Test searching"""
-    # def test_internal_search(self, browser):
-    #     internal = PoolPage(browser, Urls.POOL_INTERNAL)
-    #     internal.search_for_person(FixturesInternalPerson.first_name, FixturesInternalPerson.last_name)
-    #
-    # """Bench label filtering"""
-    # def test_internal_filter_by_label(self, browser):
-    #     internal = PoolPage(browser, Urls.POOL_INTERNAL)
-    #     internal.filter_label("Bench", tab="Internal")
-    #     internal.reset_filter_label()
-    #
-    # """Filtering by type"""
-    # @pytest.mark.parametrize('person_type',
-    #                          ("Long-term", "Short-term", "Contractor"))
-    # def test_internal_filter_by_type(self, browser, person_type):
-    #     internal = PoolPage(browser, Urls.POOL_INTERNAL)
-    #     internal.filter_type_internal(person_type)
-    #     internal.reset_filter_type_internal()
-    #
-    # """Filter by Role"""
-    # @pytest.mark.parametrize('role',
-    #                          ("SDET", "HR Manager"))
-    # def test_internal_filter_in_search_by_role(self, browser, role):
-    #     internal = PoolPage(browser, Urls.POOL_INTERNAL)
-    #     internal.filter_role(role, tab="Internal")
-    #     internal.reset_filter_role(tab="Internal")
-    #
-    # """Filter by Skills"""
-    # @pytest.mark.parametrize('skill',
-    #                          ("python", "sql"))
-    # def test_filter_in_search_by_skills(self, browser, skill):
-    #     internal = PoolPage(browser, Urls.POOL_INTERNAL)
-    #     internal.filter_skills(skill, tab="Internal", loading_time=Execute().loading_time)
-    #     internal.reset_filter_skills(tab="Internal")
-    #
-    # """Filter by city"""
-    # def test_filter_in_search_by_city(self, browser):
-    #     internal = PoolPage(browser, Urls.POOL_INTERNAL)
-    #     internal.filter_city("United States", "Los Angeles", tab="Internal", loading_time=Execute().loading_time)
-    #     internal.reset_filter_cities(tab="Internal")
-    #
-    # """Filter by office"""
-    # @pytest.mark.parametrize('office',
-    #                          ("Kazan", "Ivanovo"))
-    # def test_filter_by_office(self, browser, office):
-    #     internal = PoolPage(browser, Urls.POOL_INTERNAL)
-    #     internal.filter_office(office, tab="Internal")
-    #     internal.reset_filter_office(tab="Internal")
+    """Test searching"""
+    def test_internal_search(self, browser):
+        internal = PoolPage(browser, Urls.POOL_INTERNAL)
+        internal.search_for_person(FixturesInternalPerson.first_name, FixturesInternalPerson.last_name)
+
+    """Bench label filtering"""
+    def test_internal_filter_by_label(self, browser):
+        internal = PoolPage(browser, Urls.POOL_INTERNAL)
+        internal.filter_label("Bench", tab="Internal")
+        internal.reset_filter_label()
+
+    """Filtering by type"""
+    @pytest.mark.parametrize('person_type',
+                             ("Long-term", "Short-term", "Contractor"))
+    def test_internal_filter_by_type(self, browser, person_type):
+        internal = PoolPage(browser, Urls.POOL_INTERNAL)
+        internal.filter_type_internal(person_type)
+        internal.reset_filter_type_internal()
+
+    """Filter by Role"""
+    @pytest.mark.parametrize('role',
+                             ("SDET", "HR Manager"))
+    def test_internal_filter_in_search_by_role(self, browser, role):
+        internal = PoolPage(browser, Urls.POOL_INTERNAL)
+        internal.filter_role(role, tab="Internal")
+        internal.reset_filter_role(tab="Internal")
+
+    """Filter by Skills"""
+    @pytest.mark.parametrize('skill',
+                             ("python", "sql"))
+    def test_filter_in_search_by_skills(self, browser, skill):
+        internal = PoolPage(browser, Urls.POOL_INTERNAL)
+        internal.filter_skills(skill, tab="Internal", loading_time=Execute().loading_time)
+        internal.reset_filter_skills(tab="Internal")
+
+    """Filter by city"""
+    def test_filter_in_search_by_city(self, browser):
+        internal = PoolPage(browser, Urls.POOL_INTERNAL)
+        internal.filter_city("United States", "Los Angeles", tab="Internal", loading_time=Execute().loading_time)
+        internal.reset_filter_cities(tab="Internal")
+
+    """Filter by office"""
+    @pytest.mark.parametrize('office',
+                             ("Kazan", "Ivanovo"))
+    def test_filter_by_office(self, browser, office):
+        internal = PoolPage(browser, Urls.POOL_INTERNAL)
+        internal.filter_office(office, tab="Internal")
+        internal.reset_filter_office(tab="Internal")
 
     """Filter by Eng level"""
     @pytest.mark.parametrize('eng_level',
